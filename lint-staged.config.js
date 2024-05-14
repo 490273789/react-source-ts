8f8aa384c3f7472f937b799390fb0a84a3f0a8d6
王绍楠 <wangsn666@163.com>
2024-05-14 15:34:44 +0800

    feat: 插件升级

---
+++ lint-staged.config.js
@@ -0,0 +1,3 @@
+export default {
+  'src/**/*.{js,jsx,tsx,ts}': ["prettier --write","eslint --fix", 'git add .'],
+};
---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+export default {
+  printWidth: 80,
+  tabWidth: 2,
+  semi: false,
+  useTabs: false,
+  trailingComma: "none",
+  bracketSpacing: true
+}
